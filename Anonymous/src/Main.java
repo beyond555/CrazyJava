abda086e2af70bb9a83f72e692ae88033b9fd0fc
beyond555 <732234746@qq.com>
2014-07-21 16:36:12 +0800

    anonoymous

--- Anonymous/src/Main.java
+++ Anonymous/src/Main.java
@@ -32,5 +32,22 @@ public class Main {
 		};
 		a.showLocolVariable();
 		b.add(A, B);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i=0;i<5;i++)
+				{
+					System.out.println(local);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		}).start();
+		System.out.println("Main end");
 	}
 }
